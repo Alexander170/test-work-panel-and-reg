597682c145d329005ac01eb762d5aceebc023c67
Alexander Ershov <lancaster1@ro.ru>
2025-03-05 17:25:42 +0300

    add file module

--- src/app/Pages/dashboard/dashboard.component.ts
+++ src/app/Pages/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { DashboardModule } from './dashboard.module';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [DashboardModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
